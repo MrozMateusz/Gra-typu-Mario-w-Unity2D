fb6c9e7d266ec3644513365cf87ab800f1755f28
Daniel <daniellegawiec20@gmail.com>
2019-03-17 10:44:03 +0100

    dodanie bearTrap

---
+++ Assets/Script/traps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class traps : MonoBehaviour
+{
+    private Animator bearTrapAnimator;
+    private GameObject bearTrapGameObject;
+    private bool startBearTrap = false;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        bearTrapGameObject = GameObject.Find("bearTrap");
+        bearTrapAnimator = bearTrapGameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collider)
+    {
+        if (Collider.tag == "Player")
+        {
+            startBearTrap = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bearTrapAnimator.SetBool("bearTrapBool", startBearTrap);
+    }
+}
